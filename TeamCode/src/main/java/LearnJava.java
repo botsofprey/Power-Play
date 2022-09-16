367f9898dce274b64703acb0b7a481318d7595ef
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-09-16 16:44:55 -0400

    I did the gamepad exercises

--- TeamCode/src/main/java/LearnJava.java
+++ TeamCode/src/main/java/LearnJava.java
@@ -7,12 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class LearnJava extends OpMode {
     @Override
     public void init() {
-        String myname = "Alex Bryan";
-
-        telemetry.addData("Hello", myname);
     }
     @Override
-    public void loop(){
-
+    public void loop() {
+        telemetry.addData("Gamepad 1 Right Stick", gamepad1.right_stick_y);
+        telemetry.addData("Gamepad 1 B button", gamepad1.b);
+        double rightleft = gamepad1.left_stick_y - gamepad1.right_stick_y;
+        telemetry.addData("rightleft", rightleft);
+        double triggersum = gamepad1.right_trigger + gamepad1.left_trigger;
+        telemetry.addData("triggersum", triggersum);
     }
 }
