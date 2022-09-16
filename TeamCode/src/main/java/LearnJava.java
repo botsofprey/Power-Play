c8206a6e4856593d23c7a51870171c4550d39f6c
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-09-16 17:28:04 -0400

    I did the choices exercises

--- TeamCode/src/main/java/LearnJava.java
+++ TeamCode/src/main/java/LearnJava.java
@@ -10,11 +10,13 @@ public class LearnJava extends OpMode {
     }
     @Override
     public void loop() {
-        telemetry.addData("Gamepad 1 Right Stick", gamepad1.right_stick_y);
-        telemetry.addData("Gamepad 1 B button", gamepad1.b);
-        double rightleft = gamepad1.left_stick_y - gamepad1.right_stick_y;
-        telemetry.addData("rightleft", rightleft);
-        double triggersum = gamepad1.right_trigger + gamepad1.left_trigger;
-        telemetry.addData("triggersum", triggersum);
+        if(gamepad1.a) {
+                telemetry.addData("gamepad1 right", gamepad1.right_stick_x);
+                telemetry.addData("gamepad1 left", gamepad1.left_stick_x);
+            }
+        else if(gamepad1.a == false){
+            telemetry.addData("gamepad1 left", gamepad1.right_stick_x);
+            telemetry.addData("gamepad1 right", gamepad1.left_stick_x);
+        }
     }
 }
