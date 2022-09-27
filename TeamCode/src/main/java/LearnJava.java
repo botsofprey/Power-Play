f4f21cf4fa1694879d911edd974f2864aa3aabd9
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-09-27 14:56:22 -0400

    These are the codes for the methods and classes exercises

--- TeamCode/src/main/java/LearnJava.java
+++ TeamCode/src/main/java/LearnJava.java
@@ -3,20 +3,35 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp()
+@TeleOp
 public class LearnJava extends OpMode {
+    RobotLocation robotLocation = new RobotLocation(0);
+
     @Override
     public void init() {
+        robotLocation.setAngle(0);
     }
+
     @Override
     public void loop() {
-        if(gamepad1.a) {
-                telemetry.addData("gamepad1 right", gamepad1.right_stick_x);
-                telemetry.addData("gamepad1 left", gamepad1.left_stick_x);
-            }
-        else if(gamepad1.a == false){
-            telemetry.addData("gamepad1 left", gamepad1.right_stick_x);
-            telemetry.addData("gamepad1 right", gamepad1.left_stick_x);
+        if (gamepad1.a) {
+            robotLocation.turn(0.1);
+        } else if (gamepad1.b) {
+            robotLocation.turn(-0.1);
         }
+        if (gamepad1.dpad_left) {
+            robotLocation.changeX(-0.1);
+        } else if (gamepad1.dpad_right) {
+            robotLocation.changeX(0.1);
+        }
+        if (gamepad1.dpad_up) {
+            robotLocation.changeY(0.1);
+        } else if (gamepad1.dpad_down) {
+            robotLocation.changeY(-0.1);
+        }
+        telemetry.addData("Location", robotLocation);
+        telemetry.addData("Heading", robotLocation.getHeading());
+        telemetry.addData("X", robotLocation.getX());
+        telemetry.addData("Y", robotLocation.getY());
     }
 }
