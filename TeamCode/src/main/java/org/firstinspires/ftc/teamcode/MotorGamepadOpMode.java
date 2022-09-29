5ac6c35dddb1b146329d6d32b0bbdfc6ed6dd94f
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-09-29 13:50:18 -0400

    The motor-gamepad opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorGamepadOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorGamepadOpMode.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
-public class MotorGamepadOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ProgrammingBoard1;
+
+@TeleOp
+public class MotorGamepadOpMode extends OpMode {
+    ProgrammingBoard1 board = new ProgrammingBoard1();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double motorSpeed = gamepad1.left_stick_y;
+
+        board.setMotorSpeed(motorSpeed);
+
+        telemetry.addData("Motor rotations", board.getMotorRotations());
+        telemetry.addData("Motor rotations", board.getMotorRotations());
+    }
 }
+
+
