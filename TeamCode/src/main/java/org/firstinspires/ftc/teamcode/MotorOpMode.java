2a243512540dda065ed5c99efd34c989530643a5
Bots of Prey <pricharda273@gmail.com>
2022-10-03 15:06:46 -0400

    e

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpMode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class MotorOpMode extends OpMode{
+    ProgrammingBoard4 board = new ProgrammingBoard4();
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(board.isTouchSensorPressed()) {
+            board.setMotorSpeed(0.5);
+        }
+        else{
+            board.setMotorSpeed(0);
+        }
+        telemetry.addData("Motor rotations", board.getMotorRotations());
+    }
+}
