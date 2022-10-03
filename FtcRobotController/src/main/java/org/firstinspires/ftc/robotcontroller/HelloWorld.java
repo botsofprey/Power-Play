68f89eefc2f497b84bc6c8eb9d39885d20badea5
quin <mcdowellq@franklinacademy.org>
2022-10-03 14:59:25 -0400

    new

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.robotcontroller;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp()
+public class HelloWorld extends OpMode{
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+    @Override
+    public void loop() {
+
+    }
+}
