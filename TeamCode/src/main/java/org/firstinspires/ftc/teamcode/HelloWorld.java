537701c79c9b2e1cf59fe9ade6be869c3ae17ec3
botsofprey <thebotsofprey@gmail.com>
2022-10-03 13:52:22 -0400

    added new files to allow to change branches -Alex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -5,12 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode ;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
+//@Autonomous
 public class HelloWorld extends OpMode {
 
     @Override
     public void init() {
         //System.out.println("Hello World")
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello", "Natalia");
     }
 
     @Override
