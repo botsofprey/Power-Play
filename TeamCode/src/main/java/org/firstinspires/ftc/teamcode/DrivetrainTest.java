be54d8a7a3320c776244cef7f2d0a732677d0d9c
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-10-03 17:56:19 -0400

    The basics of a code for testing the drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.ProgrammingBoard1;
+
+@TeleOp()
+public class DrivetrainTest extends OpMode {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestProgrammingBoard.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+public class TestProgrammingBoard {
+}
