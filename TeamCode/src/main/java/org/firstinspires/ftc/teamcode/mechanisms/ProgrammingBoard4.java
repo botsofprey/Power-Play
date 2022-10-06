9ec6c0416f8119a398e9fbf01b63ec03f3d38778
botsofprey <thebotsofprey@gmail.com>
2022-10-06 11:12:40 -0400

    exercises from learn java for ftc and new packages

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard4.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ProgrammingBoard4 {
+    private DigitalChannel touchSensor;
+    private DcMotor motor;
+    private double ticksPerRotation;
+
+    public void init(HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        motor = hwMap.get(DcMotor.class, "motor");
+        ticksPerRotation = motor.getMotorType().getTicksPerRev();
+    }
+    public boolean isTouchSensorPressed() {
+        return !touchSensor.getState();
+    }
+    public void setMotorSpeed(double speed) {
+        motor.setPower(speed);
+    }
+    public double getMotorRotations(){
+        return motor.getCurrentPosition() / ticksPerRotation;
+    }
+}
