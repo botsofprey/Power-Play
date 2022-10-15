3dd7d9f92eba8110ce02051e8e28c3c12f50d102
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-10-14 20:21:20 -0400

    I made a lift OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -79,4 +78,3 @@ public class DriveTrain extends OpMode {
         }
     }
 }
-
