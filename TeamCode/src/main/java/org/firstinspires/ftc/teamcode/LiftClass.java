4643b809e74c02f127c8dd1610a0f7bc706fbf8d
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-10-18 16:21:07 -0400

    I updated the lift OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftClass.java
@@ -3,23 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp()
-public class LiftClass extends Opmode {
+public class LiftClass extends OpMode {
     DcMotor lift;
 
     @Override
     public void init() {
-        lift = hardwareMap.DcMotor.get("lift");
+        lift = hardwareMap.dcMotor.get("lift");
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
         if (gamepad1.x) {
-            resetRunTime;
-            while (getRunTime() < 5) {
+            resetStartTime();
+            while (getRuntime() < 5) {
                 lift.setPower(0.5);
             }
             lift.setPower(0);
