9f99a3e2d6a0a77198f6abcb7f48fe3ee797ee3b
botsofprey <thebotsofprey@gmail.com>
2022-11-01 16:32:59 -0400

    idk ab this but its a start

--- TeamCode/src/main/java/OpMode/Autonomous/AutoNatalia.java
+++ TeamCode/src/main/java/OpMode/Autonomous/AutoNatalia.java
@@ -9,7 +9,7 @@ import DriveEngine.MecanumDrive;
 
 @Autonomous(name="Auto Test", group = "Autonomous")
 public class AutoNatalia extends LinearOpMode {
-    ProgrammingBoard7 board = new ProgrammingBoard7()
+    ProgrammingBoard7 board = new ProgrammingBoard7();
     private MecanumDrive drive;
 
     public void runOpMode() throws InterruptedException {
@@ -19,7 +19,7 @@ public class AutoNatalia extends LinearOpMode {
     public void loop() {
         board.init(hardwareMap);
     }
-    while(opModeIsActive()){
+    while(opModeIsActive(){
         drive.moveCenti(120, MecanumDrive.LEFT);
         drive.moveCenti(480, MecanumDrive.FORWARD);
         //pick up cone
