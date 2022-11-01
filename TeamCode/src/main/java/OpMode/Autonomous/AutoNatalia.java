c6ada5b733e2fd1cf4fef423e767b73b9168b1b6
botsofprey <thebotsofprey@gmail.com>
2022-11-01 17:49:21 -0400

    idk ab this but its a start

--- TeamCode/src/main/java/OpMode/Autonomous/AutoNatalia.java
+++ TeamCode/src/main/java/OpMode/Autonomous/AutoNatalia.java
@@ -2,6 +2,8 @@ package OpMode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.ProgrammingBoard7;
 
@@ -12,6 +14,8 @@ public class AutoNatalia extends LinearOpMode {
     ProgrammingBoard7 board = new ProgrammingBoard7();
     private MecanumDrive drive;
 
+    private Servo servo;
+    private DcMotor motor;
     public void runOpMode() throws InterruptedException {
 
     }
@@ -21,12 +25,17 @@ public class AutoNatalia extends LinearOpMode {
     }
     while(opModeIsActive(){
         drive.moveCenti(120, MecanumDrive.LEFT);
+        servo.setPosition(1);
         drive.moveCenti(480, MecanumDrive.FORWARD);
+
         //pick up cone
         drive.rotateToAngle(135);
         //repeat if time allows
+        servo.setPosition(0);
+
+        motor.setMode();
 
         //no clue how to do the claw or lift
     }
 
-    }
+}
