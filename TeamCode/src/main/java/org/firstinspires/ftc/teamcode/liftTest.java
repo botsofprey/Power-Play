f060f6831ceb2535337715aa2dd34b97e302448a
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-11-04 16:51:30 -0400

    I made and cleaned up things for an encoder-controlled lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/liftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/liftTest.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
 @TeleOp()
 public class liftTest extends OpMode {
     DcMotor lift;
-    public void init(){
+
+    public void init() {
         lift = hardwareMap.dcMotor.get("lift");
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+
     public void loop() {
         telemetry.addData("Position", lift.getCurrentPosition());
     }
