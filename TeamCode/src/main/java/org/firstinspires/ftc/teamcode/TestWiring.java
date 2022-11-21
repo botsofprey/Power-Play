4bac40be2769789100480400bc4e2ff7665a2b9e
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-11-21 17:12:14 -0500

    I made a TestWiring for the rookie code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestWiring.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.ArrayList;
+
+@TeleOp()
+public class TestWiring extends OpMode {
+    HardwareMechanisms board = new HardwareMechanisms();
+    ArrayList<TestItem> tests;
+    boolean wasDown, wasUp;
+    int testNum;
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+        tests = board.getTests();
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_up && !wasUp) {
+            testNum--;
+            if (testNum < 0) {
+                testNum = tests.size() - 1;
+            }
+        }
+        wasUp = gamepad1.dpad_up;
+        if (gamepad1.dpad_down && !wasDown) {
+            testNum++;
+            if (testNum >= tests.size()) {
+                testNum = 0;
+            }
+        }
+        wasDown = gamepad1.dpad_down;
+        telemetry.addLine("Use Up and Down on D-pad to cycle through choices");
+        telemetry.addLine("Press A to run test");
+        TestItem currTest = tests.get(testNum);
+        telemetry.addData("Test:", currTest.getDescription());
+        currTest.run(gamepad1.a, telemetry);
+    }
+}
