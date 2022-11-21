16ab90b506396d191ace3baa6ba33bf8b3553304
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-11-21 17:12:14 -0500

    I made a TestWiring for the rookie code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMechanisms.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMechanisms.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import java.util.ArrayList;
+
 public class HardwareMechanisms {
     DcMotor motorFrontRight;
     DcMotor motorBackRight;
@@ -41,11 +43,17 @@ public class HardwareMechanisms {
         lift.setPower(liftSpeed);
     }
 
-    public Servo getClaw() {
-        return claw;
-    }
-
     public void setClaw(double clawPosit) {
         claw.setPosition(clawPosit);
+    }
+
+    public ArrayList<TestItem> getTests() {
+        ArrayList<TestItem> tests = new ArrayList<>();
+        tests.add(new TestMotor("FrontRight", 0.5, motorFrontRight));
+        tests.add(new TestMotor("FrontLeft", 0.5, motorFrontLeft));
+        tests.add(new TestMotor("BackRight", 0.5, motorBackRight));
+        tests.add(new TestMotor("BackLeft", 0.5, motorBackLeft));
+        tests.add(new TestServo("Claw", claw, 0.0, 1.0));
+        return tests;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class TestServo extends TestItem {
+    private Servo servo;
+    double onValue;
+    double offValue;
+
+    public TestServo(String description, Servo servo, double offValue, double onValue) {
+        super(description);
+        this.servo = servo;
+        this.onValue = onValue;
+        this.offValue = offValue;
+    }
+
+    @Override
+    public void run(boolean on, Telemetry telemetry) {
+        if (on) {
+            servo.setPosition(onValue);
+        } else {
+            servo.setPosition(offValue);
+        }
+    }
+
+}
