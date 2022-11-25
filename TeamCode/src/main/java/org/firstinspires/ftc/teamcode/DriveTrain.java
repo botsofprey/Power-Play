f2a300ebbfa8e3f52f37be2508a47bc820470bf7
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-11-25 14:13:57 -0500

    I limited acceleration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -13,6 +13,7 @@ public class DriveTrain extends OpMode {
     DcMotor motorFrontLeft;
 
     double limitPowerChange = 0.05;
+    double limitPowerChangeFast = 0.01;
     double lastX;
     double lastRx;
     double lastY;
@@ -38,27 +39,48 @@ public class DriveTrain extends OpMode {
         double y = -gamepad1.left_stick_y;
         double x = gamepad1.left_stick_x * 1.1;
         double rx = gamepad1.right_stick_x;
-
         double changeX = x - lastX;
-        if (Math.abs(changeX) > limitPowerChange) {
-            changeX = Math.signum(changeX) * limitPowerChange;
-        }
-        x = lastX + changeX;
-        lastX = x;
+        if (gamepad1.left_bumper) {
+            if (Math.abs(changeX) > limitPowerChangeFast) {
+                changeX = Math.signum(changeX) * limitPowerChangeFast;
+            }
+            x = lastX + changeX;
+            lastX = x;
 
-        double changeRX = rx - lastRx;
-        if (Math.abs(changeRX) > limitPowerChange) {
-            changeRX = Math.signum(changeRX) * limitPowerChange;
-        }
-        rx = lastRx + changeRX;
-        lastRx = rx;
+            double changeRX = rx - lastRx;
+            if (Math.abs(changeRX) > limitPowerChangeFast) {
+                changeRX = Math.signum(changeRX) * limitPowerChangeFast;
+            }
+            rx = lastRx + changeRX;
+            lastRx = rx;
 
-        double changeY = y - lastY;
-        if (Math.abs(changeY) > limitPowerChange) {
-            changeY = Math.signum(changeY) * limitPowerChange;
+            double changeY = y - lastY;
+            if (Math.abs(changeY) > limitPowerChangeFast) {
+                changeY = Math.signum(changeY) * limitPowerChangeFast;
+            }
+            y = lastY + changeY;
+            lastY = y;
+        } else {
+            if (Math.abs(changeX) > limitPowerChange) {
+                changeX = Math.signum(changeX) * limitPowerChange;
+            }
+            x = lastX + changeX;
+            lastX = x;
+
+            double changeRX = rx - lastRx;
+            if (Math.abs(changeRX) > limitPowerChange) {
+                changeRX = Math.signum(changeRX) * limitPowerChange;
+            }
+            rx = lastRx + changeRX;
+            lastRx = rx;
+
+            double changeY = y - lastY;
+            if (Math.abs(changeY) > limitPowerChange) {
+                changeY = Math.signum(changeY) * limitPowerChange;
+            }
+            y = lastY + changeY;
+            lastY = y;
         }
-        y = lastY + changeY;
-        lastY = y;
 
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double frontLeftPower = (y + x + rx) / denominator;
