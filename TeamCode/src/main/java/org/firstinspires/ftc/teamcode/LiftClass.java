e07b1cd3f2fdafae85bdf17055178296152f10b5
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2022-12-01 18:36:04 -0500

    I made a test code for preset heights on the lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftClass.java
@@ -1,21 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp()
 public class LiftClass extends OpMode {
     DcMotor lift;
+    double targetPosition;
+    double lowJunction;
+    double midJunction;
+    double highJunction;
 
     @Override
     public void init() {
         lift = hardwareMap.dcMotor.get("lift");
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
-        lift.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+        if (gamepad2.a) {
+            if (targetPosition == 0) {
+                targetPosition = lowJunction;
+            } else if (targetPosition == lowJunction) {
+                targetPosition = midJunction;
+            } else if (targetPosition == midJunction) {
+                targetPosition = highJunction;
+            }
+        } else if (gamepad2.b) {
+            if (targetPosition == highJunction) {
+                targetPosition = midJunction;
+            } else if (targetPosition == midJunction) {
+                targetPosition = lowJunction;
+            } else if(targetPosition == lowJunction){
+                targetPosition = 0;
+            }
+        }
+        if(lift.getCurrentPosition() < targetPosition){
+            lift.setPower(0.5);
+        } else if(lift.getCurrentPosition() > targetPosition){
+            lift.setPower(-0.5);
+        }
     }
 }
