01e7aba3ca9dbcd76039a569f26d0c8e96290fe5
botsofprey <thebotsofprey@gmail.com>
2022-12-10 10:16:59 -0500

    added a lift reset

--- TeamCode/src/main/java/Subsystems/Lift.java
+++ TeamCode/src/main/java/Subsystems/Lift.java
@@ -2,8 +2,10 @@ package Subsystems;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.ProgrammingBoard7;
 
@@ -12,79 +14,77 @@ public class Lift {
 
     private boolean braking;
 
-    public Lift (HardwareMap hardwareMap){
+    private TouchSensor limitSwitch;
+
+    public Lift(HardwareMap hardwareMap) {
         liftMotor = hardwareMap.get(DcMotor.class, "lift");
+        limitSwitch = hardwareMap.get(TouchSensor.class, "limitSwitch");
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void setPower(double power){
-            liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            liftMotor.setPower(power);
+    public void setPower(double power) {
+        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftMotor.setPower(power);
     }
 
-    public void setPosition(int position, double power){
+    public void setPosition(int position, double power) {
         liftMotor.setTargetPosition(position);
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         liftMotor.setPower(power);
     }
 
-    /*public void setAPosition(double position, double power) {
-        liftMotor.setTargetPosition(position);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(power);
-    }*/
-
-    public int getPosition(){
+    public int getPosition() {
         return liftMotor.getCurrentPosition();
     }
 
-    public void brake(){
+    public void brake() {
         liftMotor.setTargetPosition(liftMotor.getCurrentPosition());
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         liftMotor.setPower(.5);
     }
 
-    public void zeroLift(){
+    public void zeroLift() {
         liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    //here goes set positions
+    public void Bottom(int position, double power) {
+        liftMotor.setTargetPosition(0);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(0.75);
+    }
+
+    public void Quarter(int position, double power) {
+        liftMotor.setTargetPosition(6);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(0.75);
+    }
+
+    public void Half(int position, double power) {
+        liftMotor.setTargetPosition(12);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(0.75);
+    }
+
+    public void ThreeQuarters(int position, double power) {
+        liftMotor.setTargetPosition(27);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(0.75);
+    }
+
+    public void Top(int position, double power) {
+        liftMotor.setTargetPosition(36);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(0.75);
     }
 
     public void update(HardwareMap hwMap) {
         ProgrammingBoard7 board = new ProgrammingBoard7();
         telemetry.addData("Touch Sensor", board.getClass());
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
-    //here goes set positions
-    public void Bottom (int position, double power) {
-        liftMotor.setTargetPosition(0);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(0.75);
-    }
-    public void Quarter (int position, double power) {
-        liftMotor.setTargetPosition(6);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(0.75);
-    }
-
-    public void Half  (int position, double power) {
-        liftMotor.setTargetPosition(12);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(0.75);
-    }
-
-    public void ThreeQuarters (int position, double power){
-        liftMotor.setTargetPosition(27);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(0.75);
-    }
-
-    public void Top (int position, double power) {
-        liftMotor.setTargetPosition(36);
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setPower(0.75);
-    }
-
-    public void update () {
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        if (limitSwitch.isPressed()) {
+            zeroLift();
+        }
     }
 }
