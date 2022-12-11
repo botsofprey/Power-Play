4a9ac8f9bf8dfebcadcb0c8fe89b4373830d8712
botsofprey <thebotsofprey@gmail.com>
2022-12-11 10:42:04 -0500

    auton

--- TeamCode/src/main/java/OpMode/Autonomous/CornerPark.java
+++ TeamCode/src/main/java/OpMode/Autonomous/CornerPark.java
@@ -3,18 +3,23 @@ package OpMode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import DriveEngine.MecanumDrive;
 import Subsystems.threeWheelOdometry;
+import UtilityClasses.Location;
 
 @Autonomous(name="Auto Test2", group = "Autonomous")
 public abstract class CornerPark extends LinearOpMode {
     private MecanumDrive drive;
     private threeWheelOdometry odometry;
-    private DcMotor motor;
+
     public void runOpMode (){
+        drive = new MecanumDrive(hardwareMap,0);
+        odometry = new threeWheelOdometry (hardwareMap, new Location(0,0), this, drive);
+
         if(opModeIsActive()){
-            drive.moveCenti(36, MecanumDrive.FORWARD);
+            drive.moveCenti(65, MecanumDrive.FORWARD);
             while(opModeIsActive() && drive.isBusy()
             ) {
 
