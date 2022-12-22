65ed1dfcd40b71380773f0658451aab22921df72
botsofprey <thebotsofprey@gmail.com>
2022-12-22 10:20:19 -0500

    very very wrong road runner

---
+++ TeamCode/src/main/java/Subsystems/AutoTransferPose.java
@@ -0,0 +1,36 @@
+package Subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import UtilityClasses.Vector2D;
+
+public class AutoTransferPose extends LinearOpMode {
+    @Override
+
+    public void runOpMore() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(10, 15, Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Trajectory traj = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2D(45, 45), 0)
+                .build();
+
+        drive.followTrajectory(traj);
+
+        sleep(2000);
+
+        drive.followTrajectory(
+                drive.trajectoryBuilder(traj.end(), true)
+                        .splineTo(new Vector2D(15, 15), Math.toRadians(180))
+                        .build()
+        );
+        PoseStorage.currentPose = drive.getPoseEstimate();
+    }
+}
