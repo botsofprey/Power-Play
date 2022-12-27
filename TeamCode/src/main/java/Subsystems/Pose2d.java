c4232d659257d7c266c6e20e5bdf6dd8938d26b7
botsofprey <thebotsofprey@gmail.com>
2022-12-27 11:33:07 -0500

    wrong road runner

--- TeamCode/src/main/java/Subsystems/Pose2d.java
+++ TeamCode/src/main/java/Subsystems/Pose2d.java
@@ -1,15 +1,15 @@
 package Subsystems;
 
 public class Pose2d {
-   Pose2d myPose = new Pose2d(10, -5, Math.toRadians(90));
+    Pose2d myPose = new Pose2d(10, -5, Math.toRadians(90));
 
     public Pose2d(int i, int i1, double toRadians) {
     }
-    
+
     public Pose2d(int i, double x, int i1) {
     }
-    
-    public Pose2d(){
+
+    public Pose2d() {
     }
 
     public Pose2d(double forwardOffset, int i, double toRadians) {
--- TeamCode/src/main/java/Subsystems/StandardTrackngWheelLocalizer.java
+++ TeamCode/src/main/java/Subsystems/StandardTrackngWheelLocalizer.java
@@ -15,7 +15,7 @@ import org.openftc.easyopencv.PipelineRecordingParameters;
 import java.util.Arrays;
 import java.util.Base64;
 
-public class StandardTrackngWheelLocalizer extends ThreeTrackingWheelLocalizer{
+public class StandardTrackngWheelLocalizer extends ThreeTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 0;
     public static double WHEEL_RADIUS = 2;
     public static double GEAR_RATIO = 1;
@@ -24,18 +24,20 @@ public class StandardTrackngWheelLocalizer extends ThreeTrackingWheelLocalizer{
     public static double FORWARD_OFFSET = 2; //idk how to measure it
 
     private Base64.Encoder leftEncoder, rightEncoder, frontEncoder;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     public StandardTrackngWheelLocalizer(HardwareMap hardwareMap) throws Throwable {
         super(Arrays.asList(
-        new Pose2d(0, LATERAL_DISTANCE / 2,0),
-        new Pose2d(0,-LATERAL_DISTANCE / 2,0),
-        new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90))
+                new Pose2d(0, LATERAL_DISTANCE / 2, 0),
+                new Pose2d(0, -LATERAL_DISTANCE / 2, 0),
+                new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90))
         ));
 
         //leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "leftEncoder"));
         //rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightEncoder"));
         //frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontEncoder"));
     }
+
     public static double encoderTicksToInches(double ticks) {
         return WHEEL_RADIUS * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
     }
