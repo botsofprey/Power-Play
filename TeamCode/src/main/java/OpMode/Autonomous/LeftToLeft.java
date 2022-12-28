0e0aff5eba926e601c4430ceb0a7ca841ec33352
botsofprey <thebotsofprey@gmail.com>
2022-12-28 17:46:35 -0500

    correct road runner?

---
+++ TeamCode/src/main/java/OpMode/Autonomous/LeftToLeft.java
@@ -0,0 +1,24 @@
+/*This teleop is for when the robot begins on the left/blue square-ish thing (audience
+orentation)and is trying to go to the left same colored ?storage? (idk man whatever scores the
+points)
+ *//*
+thought process
+scan qr code
+pick up THE cone
+score it or just bring it with you to the storage triangle thing
+     (if scoring prolly on the medium or low or even ground junction)
+reverse back to the original starting position
+go left into the half square
+wait til the drivers drive around
+ta-da all done
+*/
+//but you should ignore this because its prolly wrong
+
+package OpMode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class LeftToLeft {
+
+}
