62d5869ff6a757eedaa437eda10b441cfba6e955
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-12 18:03:50 -0500

    I'm working on PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -7,16 +7,15 @@ public class PID {
     final private double kI = 0;
     final private double Kd = 0;
     private double totalI;
-    final private double cycleTime = 1;
     private double lastError;
 
     public void PidControl(int position, DcMotor motor) {
         double time = System.currentTimeMillis();
         double error = position - motor.getCurrentPosition();
         double p = kP * error;
-        double i = kI * error * cycleTime;
+        double i = kI * error * time;
         totalI += i;
-        double d = Kd * (error - lastError) / cycleTime;
+        double d = Kd * (error - lastError) / time;
         motor.setPower(p + totalI + d);
         lastError = position - motor.getCurrentPosition();
     }
