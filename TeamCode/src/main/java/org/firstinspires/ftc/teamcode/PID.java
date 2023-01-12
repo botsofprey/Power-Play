f3a52d26bd07e26669449f103fc0c5b7a5656baf
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-12 17:15:14 -0500

    I'm working on PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -3,9 +3,20 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class PID {
+    final double kP = 0;
+    final private double kI = 0;
+    final private double Kd = 0;
+    private double totalI;
+    private double lastError;
 
     public void PidControl(int position, DcMotor motor) {
         double time = System.currentTimeMillis();
         double error = position - motor.getCurrentPosition();
+        double p = kP * error;
+        double i = kI * error * time;
+        totalI += i;
+        double d = Kd * (error - lastError) / time;
+        motor.setPower(p + totalI + d);
+        lastError = position - motor.getCurrentPosition();
     }
 }
