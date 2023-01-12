c44f338ffe456369657fd4cd9c47d1c404810bf2
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-12 17:02:28 -0500

    I'm working on PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class PID {
 
-    public void PidControl(int position) {
+    public void PidControl(int position, DcMotor motor) {
         double time = System.currentTimeMillis();
+        double error = position - motor.getCurrentPosition();
     }
 }
