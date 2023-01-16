29a4e66f005e1e22803a0a6f6b77b58d3ca41ed1
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-16 13:37:46 -0500

    Roadrunner tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoordinateLocations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoordinateLocations.java
@@ -6,12 +6,11 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 //the coordinate plane is from the red alliance's perspective
 public class CoordinateLocations {
    //robot starting positions:
-   double startX;
    double startY = 63.75;
-   Pose2d leftBlueStart = new Pose2d(startX, startY, 180); //Quadrant 1
-   Pose2d rightBlueStart = new Pose2d(-startX, startY, 180); //Quadrant 2
-   Pose2d leftRedStart = new Pose2d(-startX, -startY, 0); //Quadrant 3
-   Pose2d rightRedStart = new Pose2d(startX, -startY, 0); //Quadrant 4
+   Pose2d leftBlueStart = new Pose2d(0, startY, 180); //Quadrant 1
+   Pose2d rightBlueStart = new Pose2d(-0, startY, 180); //Quadrant 2
+   Pose2d leftRedStart = new Pose2d(-0, -startY, 0); //Quadrant 3
+   Pose2d rightRedStart = new Pose2d(0, -startY, 0); //Quadrant 4
    //cone stack coordinates:
    Vector2d leftBlueConeStack = new Vector2d(66, -12); //Quadrant 1
    Vector2d rightBlueConeStack = new Vector2d(-66,12); //Quadrant 2
