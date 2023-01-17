d6ec7de9a75eaa5bfb8ed9853bb4808331dc2c80
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-17 16:45:46 -0500

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vars/HeightsList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vars/HeightsList.java
@@ -11,41 +11,41 @@ public class HeightsList { //heights are all in tics
     /**
      * An int used to represent the tic value of the lift at the height of the low junction, it is subject to change based off of the lift
      */
-    public int lowJunction = 683;
+    public int lowJunction = (int) Math.round(683 * spoolRatio);
     /**
      * An int used to represent the tic value of the lift at the height of the medium junction, it is subject to change based off of the lift
      */
-    public int midJunction = 900;
+    public int midJunction = (int) Math.round(900 * spoolRatio);
     /**
      * An int used to represent the tic value of the lift at the height of the high junction, it is subject to change based off of the lift
      */
-    public int highJunction = 1366;
+    public int highJunction = (int) Math.round(1366 * spoolRatio);
     /**
      * An int used to represent the tic value at the the height of the ground junction, it is subject to change based of the lift
      */
-    public int groundJunction = 400;
+    public int groundJunction = (int) Math.round(400 * spoolRatio);
 
     //cone stack heights:
     /**
      * An int used to represent the tic value of the lift at the height of the cone stack with five cones, it is subject to change based of the lift
      */
-    int fiveConeStack = 1316;
+    int fiveConeStack = (int) Math.round(1316 * spoolRatio);
     /**
      * An int used to represent the tic value of the lift at the height of the cone stack with four cones, it is subject to change based of the lift
      */
-    int fourConeStack = 1260;
+    int fourConeStack = (int) Math.round(1260 * spoolRatio);
     /**
      * An int used to represent the tic value of the lift at the height of cone stack with three cones, it is subject to change based of the lift
      */
-    int threeConeStack = 1148;
+    int threeConeStack = (int) Math.round(1148 * spoolRatio);
     /**
      * An int used to represent the tic value of the lift at the height of the cone stack with two cones, it is subject to change based of the lift
      */
-    int twoConeStack = 852;
+    int twoConeStack = (int) Math.round(852 * spoolRatio);
     /**
      * An int used to represent the tic value of the lift at the height of one cone, it is subject to change based of the lift
      */
-    int oneConeStack = 723;
+    int oneConeStack = (int) Math.round(723 * spoolRatio);
 
     //extra height:
     public int rightAboveACone = 0; //replace this number with the real value
