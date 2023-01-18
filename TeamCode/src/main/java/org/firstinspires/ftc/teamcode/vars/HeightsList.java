33199df9f3f904b5b2e297fb581c11f22ff982e8
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-17 19:02:58 -0500

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vars/HeightsList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vars/HeightsList.java
@@ -6,7 +6,7 @@ package org.firstinspires.ftc.teamcode.vars;
 public class HeightsList { //heights are all in tics
 //calculation values:
     double currentSpoolDiameter = 47.2;
-    double spoolRatio = (Math.pow(47.2, 2) * Math.PI) / (Math.pow(currentSpoolDiameter, 2) * Math.PI);
+    double spoolRatio = 47.2 / currentSpoolDiameter;
     //junction heights:
     /**
      * An int used to represent the tic value of the lift at the height of the low junction, it is subject to change based off of the lift
