7b17a5f1fb3d183b282ff3356f4729a8dbeee4d9
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-01-24 18:08:20 -0500

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vars/HeightsList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vars/HeightsList.java
@@ -15,41 +15,34 @@ public class HeightsList { //heights are all in tics
     /**
      * An int used to represent the tic value of the lift at the height of the low junction, it is subject to change based off of the lift
      */
-    public int lowJunction = (int) Math.round(430 * ratio);
-    /**
+    public int lowJunction = (int) Math.round(430 * ratio), /**
      * An int used to represent the tic value of the lift at the height of the medium junction, it is subject to change based off of the lift
      */
-    public int midJunction = (int) Math.round(685 * ratio);
-    /**
+    midJunction = (int) Math.round(685 * ratio), /**
      * An int used to represent the tic value of the lift at the height of the high junction, it is subject to change based off of the lift
      */
-    public int highJunction = (int) Math.round(950 * ratio);
-    /**
+    highJunction = (int) Math.round(950 * ratio), /**
      * An int used to represent the tic value at the the height of the ground junction, it is subject to change based of the lift
      */
-    public int groundJunction = (int) Math.round(190 * ratio);
+    groundJunction = (int) Math.round(190 * ratio);
 
     //cone stack heights:
     /**
      * An int used to represent the tic value of the lift at the height of the cone stack with five cones, it is subject to change based of the lift
      */
-    int fiveConeStack = (int) Math.round(160 * ratio);
-    /**
+    int fiveConeStack = (int) Math.round(160 * ratio), /**
      * An int used to represent the tic value of the lift at the height of the cone stack with four cones, it is subject to change based of the lift
      */
-    int fourConeStack = (int) Math.round(100 * ratio);
-    /**
+    fourConeStack = (int) Math.round(100 * ratio), /**
      * An int used to represent the tic value of the lift at the height of cone stack with three cones, it is subject to change based of the lift
      */
-    int threeConeStack = (int) Math.round(95 * ratio);
-    /**
+    threeConeStack = (int) Math.round(95 * ratio), /**
      * An int used to represent the tic value of the lift at the height of the cone stack with two cones, it is subject to change based of the lift
      */
-    int twoConeStack = (int) Math.round(60 * ratio);
-    /**
+    twoConeStack = (int) Math.round(60 * ratio), /**
      * An int used to represent the tic value of the lift at the height of one cone, it is subject to change based of the lift
      */
-    int oneConeStack = (int) Math.round(0 * ratio);
+    oneConeStack = (int) Math.round(0 * ratio);
 
     public int[] heights = {fiveConeStack, fourConeStack, threeConeStack, twoConeStack, oneConeStack};
 
