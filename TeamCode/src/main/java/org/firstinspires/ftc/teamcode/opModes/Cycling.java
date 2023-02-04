6bb2279aea6c67b594689a52eee0eda224d4bfa0
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-02-03 20:15:20 -0500

    I fixed the MeepMeep simulation bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Cycling.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Cycling.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -24,37 +23,23 @@ public class Cycling extends OpMode {
         drive = new SampleMecanumDrive(hardwareMap);
         drive.setPoseEstimate(new Pose2d(-15, 63, Math.toRadians(90)));
         cycle = drive.trajectorySequenceBuilder(new Pose2d(-15, 63, Math.toRadians(90)))
-                .addTemporalMarker(() -> {
-                    board.setClaw(0.4);
-                })
-                .waitSeconds(0.5)
-                .addTemporalMarker(() -> {
+                .addTemporalMarker(()->{
                     liftPosition = heightsList.highJunction;
                 })
-                .waitSeconds(0.1)
-                .lineToConstantHeading(new Vector2d(-12, 60))
-                .lineToSplineHeading(new Pose2d(-15, 36, 200))
-                .lineToConstantHeading(new Vector2d(-14, 30))
-                .splineToSplineHeading(new Pose2d(-18.5, 6, Math.toRadians(225)), Math.toRadians(90))
-                .addTemporalMarker(() -> {
-                    board.setClaw(0);
-                })
                 .waitSeconds(0.5)
-                .lineToConstantHeading(new Vector2d(-12, 10))
-                .addTemporalMarker(() -> {
-                    liftPosition = heightsList.rightAboveACone;
-                })
-                .waitSeconds(0.5)
-                .lineToSplineHeading(new Pose2d(-14, 30, Math.toRadians(180)))
-                .lineToSplineHeading(new Pose2d(-13, 50, Math.toRadians(90)))
-                .lineToConstantHeading(new Vector2d(-15, 63))
+                .lineToLinearHeading(new Pose2d(-15, 12, Math.toRadians(90)))
+                .turn(Math.toRadians(-45))
+                .lineToLinearHeading(new Pose2d(-5, 18, Math.toRadians(135)))
+//                .addTemporalMarker(()->{})
+//                .waitSeconds(0.5)
                 .build();
+        drive.followTrajectorySequenceAsync(cycle);
     }
 
     @Override
     public void loop() {
-        drive.followTrajectorySequenceAsync(cycle);
         drive.update();
         board.setLift(liftPosition);
+
     }
 }
