7944493fc9da6f3b27b7f3b79d44f807213b9d10
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-02-10 20:27:32 -0500

    Some changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TestDeadwheels.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.HardwareMechanisms;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.StandardTrackingWheelLocalizer;
+
+@TeleOp
+public class TestDeadwheels extends OpMode {
+    Double[] wheelValues = new Double[3];
+    SampleMecanumDrive drive;
+    HardwareMechanisms board = new HardwareMechanisms();
+    StandardTrackingWheelLocalizer wheels;
+
+    @Override
+    public void init() {
+        drive = new SampleMecanumDrive(hardwareMap);
+        wheels = new StandardTrackingWheelLocalizer(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        wheels.getWheelPositions().toArray(wheelValues);
+        telemetry.addData("Left Encoder", wheelValues[0]);
+        telemetry.addData("Right Encoder", wheelValues[1]);
+        telemetry.addData("Center Encoder", wheelValues[2]);
+    }
+}
