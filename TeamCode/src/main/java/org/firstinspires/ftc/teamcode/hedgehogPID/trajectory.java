10a8472ed8ac883ae2393c8836b269cf84d94fba
quin <mcdowellq@franklinacademy.or>
2023-02-13 18:50:42 -0500

    Merge remote-tracking branch 'origin/TillTheWheelsFallOff' into TillTheWheelsFallOff
    # Conflicts:
    #	build.gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hedgehogPID/trajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hedgehogPID/trajectory.java
@@ -7,6 +7,8 @@ import static org.firstinspires.ftc.teamcode.hedgehogPID.driveConstants.COMPLETE
 import static org.firstinspires.ftc.teamcode.hedgehogPID.driveConstants.MAX_VELOCITY;
 import static org.firstinspires.ftc.teamcode.hedgehogPID.driveConstants.TICKS_PER_REV;
 
+import androidx.annotation.Nullable;
+
 import java.util.List;
 
 public class trajectory {
@@ -21,15 +23,9 @@ public class trajectory {
 
     public trajectory setSegment(double forbackBLOCK, double strafeBLOCK, double turnDEG,
                            double targetVelocityBLOCK, marker marker) {
-        double frontLeftMotor  = 0.0,
-               frontRightMotor = 0.0,
-               backLeftMotor   = 0.0,
-               backRightMotor  = 0.0;
+        double[] motors = initMotors();
 
         double targetVelocity = Math.min((targetVelocityBLOCK * TICKS_PER_REV), MAX_VELOCITY);
-
-        double[] motors = new double[]{frontLeftMotor, frontRightMotor,
-                                       backLeftMotor, backRightMotor};
 
         double duration = Math.hypot(strafeBLOCK * TICKS_PER_REV, forbackBLOCK * TICKS_PER_REV)
                           / (targetVelocity / dm.driveMode);
@@ -37,22 +33,43 @@ public class trajectory {
         //convert degrees into parts of a whole turn
         turnDEG = (turnDEG / 360) * (TICKS_PER_REV * COMPLETE_TURN_ROTS);
 
-        //turn robot a certain amount of degrees
-        motors[0] = motors[2] -= (turnDEG * dm.driveMode);
-        motors[1] = motors[3] += (turnDEG * dm.driveMode);
+        addTurn(turnDEG, motors);
+        addStrafe(strafeBLOCK, motors);
+        addForback(forbackBLOCK, motors);
 
-        //Move forward or backward a certain amount of blocks
-        for (int i = 0; i < 3; i++)
-            motors[i] += (forbackBLOCK * dm.driveMode);
-
-        //Strafe a certain amount of blocks
-        motors[1] = motors[2] += (strafeBLOCK * dm.driveMode * BLOCK_LENGTH);
-        motors[0] = motors[3] -= (strafeBLOCK * dm.driveMode * BLOCK_LENGTH);
-
-        //return calculation results and
+        //return calculation results
         segment seg = new segment(motors, duration, targetVelocity);
         segmentList.add(seg);
         return this;
+    }
+
+    public double[] addTurn(double degrees, double motors[]) {
+        motors[0] = motors[2] -= (degrees * dm.driveMode);
+        motors[1] = motors[3] += (degrees * dm.driveMode);
+        return motors;
+    }
+
+    public double[] addStrafe(double strafe, double motors[]) {
+        motors[1] = motors[2] += (strafe * dm.driveMode * BLOCK_LENGTH);
+        motors[0] = motors[3] -= (strafe * dm.driveMode * BLOCK_LENGTH);
+        return motors;
+    }
+
+    public double[] addForback(double forback, double motors[]) {
+        for (int i = 0; i < 3; i++)
+            motors[i] += (forback * dm.driveMode);
+        return motors;
+    }
+
+    public
+
+    public double[] initMotors() {
+        double frontLeft  = 0.0,
+               frontRight = 0.0,
+               backLeft   = 0.0,
+               backRight  = 0.0;
+        return new double[]{frontLeft, frontRight,
+                            backLeft,  backRight };
     }
 
     public void executeSegmentList() {
