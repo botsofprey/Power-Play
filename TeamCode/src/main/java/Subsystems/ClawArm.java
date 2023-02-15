a059cce9816b07946423c2de9252deba8e98e35f
botsofprey <thebotsofprey@gmail.com>
2023-02-14 19:53:10 -0500

    new robot teleop

--- TeamCode/src/main/java/Subsystems/ClawArm.java
+++ TeamCode/src/main/java/Subsystems/ClawArm.java
@@ -1,5 +1,6 @@
 package Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -7,6 +8,9 @@ public class ClawArm {
     public static final double UP_POSITION = 1, DOWN_POSITION = 0;
     private Servo servoLeft, servoRight;
     private Servo servoWrist;
+
+    private DcMotorEx turrent;
+
     public ClawArm(HardwareMap hardwareMap) {
          servoLeft = hardwareMap.get(Servo.class, "Left servo");
          servoRight = hardwareMap.get(Servo.class, "Right servo");
@@ -22,4 +26,15 @@ public class ClawArm {
         servoWrist.setPosition(position);
     }
 
+    public void flipWrist(){
+        servoWrist.setPosition(servoWrist.getPosition() != 1 ? 1 : 0);
+    }
+
+    public void setTurrentPower(double power){
+        turrent.setPower(power);
+    }
+
+    public boolean turrentIn(){
+        return turrent.getCurrentPosition() <= 0;
+    }
 }
