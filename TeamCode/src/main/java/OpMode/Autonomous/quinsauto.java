d636c58f89d7280012e46568acf2cf52d32f2e09
quin <mcdowellq@franklinacademy.or>
2023-02-16 18:50:07 -0500

    Merge remote-tracking branch 'origin/TillTheWheelsFallOff' into TillTheWheelsFallOff
    # Conflicts:
    #	MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java

---
+++ TeamCode/src/main/java/OpMode/Autonomous/quinsauto.java
@@ -0,0 +1,14 @@
+package OpMode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class quinsauto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
