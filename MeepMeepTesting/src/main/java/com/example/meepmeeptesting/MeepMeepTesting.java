83388f9011ff15cba53726c9a59bb8ff642ab0c7
quin <mcdowellq@franklinacademy.or>
2023-02-16 19:09:44 -0500

    Merge remote-tracking branch 'origin/TillTheWheelsFallOff' into TillTheWheelsFallOff
    # Conflicts:
    #	MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -24,77 +24,13 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                .setDimensions(13, 16.5)
+                .setDimensions(13, 18)
                 .setConstraints(60, 40, 10, Math.toRadians(180), 10.5553)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-40.5,64.5,270))
-                                //preload
-                                .addSpatialMarker(new Vector2d(-40.6, 64), () -> {})
-                                .lineToLinearHeading(new Pose2d(-36, 64.5, toRAD(270)))
-                                .lineToSplineHeading(new Pose2d(-36, 30, toRAD(270)))
-                                .splineToSplineHeading(new Pose2d(-24 - 6, 6, toRAD(270 + 45)), toRAD(270 + 45))
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)
-
-                                //getconeandscore
-                                .back(4)
-                                .splineToLinearHeading(new Pose2d(-36, 12, toRAD(180)), toRAD(180))
-                                .lineToSplineHeading(new Pose2d(-60, 12, toRAD(180)))
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)
-                                .lineToSplineHeading(new Pose2d(-36, 12, toRAD(180)))
-                                .splineToLinearHeading(new Pose2d(-24 - 9, 9, toRAD(270 + 45)), toRAD(270+ 45))
-                                .splineToLinearHeading(new Pose2d(-24 - 6, 6, toRAD(270 + 45)), toRAD(270+ 45))
-
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)
-
-                                //getconeandscore2
-                                .back(4)
-                                .splineToLinearHeading(new Pose2d(-36, 12, toRAD(180)), toRAD(180))
-                                .lineToSplineHeading(new Pose2d(-64, 12, toRAD(180)))
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)
-                                .lineToSplineHeading(new Pose2d(-36, 12, toRAD(180)))
-                                .splineToLinearHeading(new Pose2d(-24 - 9, 9, toRAD(270 + 45)), toRAD(270+ 45))
-                                .splineToLinearHeading(new Pose2d(-24 - 6, 6, toRAD(270 + 45)), toRAD(270+ 45))
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)
-
-                                //getconeandscore3
-                                /**/.back(4)
-                                .splineToLinearHeading(new Pose2d(-36, 12, toRAD(180)), toRAD(180))
-                                .lineToSplineHeading(new Pose2d(-64, 12, toRAD(180)))
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)
-                                .lineToSplineHeading(new Pose2d(-36, 12, toRAD(180)))
-                                .splineToLinearHeading(new Pose2d(-24 - 9, 9, toRAD(270 + 45)), toRAD(270+ 45))
-                                .splineToLinearHeading(new Pose2d(-24 - 6, 6, toRAD(270 + 45)), toRAD(270+ 45))
-
-                                .waitSeconds(0.25)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.25)/**/
-                                /*.back(3)
-                                .splineToLinearHeading(new Pose2d(-36, 12, toRAD(180)), toRAD(180))
-                                .lineToSplineHeading(new Pose2d(-64, 12, toRAD(180)))
-                                .waitSeconds(0.5)
-                                .addTemporalMarker(() -> {})
-                                .waitSeconds(0.5)
-                                .lineToLinearHeading(new Pose2d(-48, 12, toRAD(90)))
-                                .forward(5)
-                                .waitSeconds(1)
-                                .back(5)*/
-
-                                //park17
-                                .lineToLinearHeading(new Pose2d(-36, 12, toRAD(270 + 45)))
-                                .turn(toRAD(-45))
-                                .lineToLinearHeading(new Pose2d(-12,12, toRAD(270)))
+                        drive.trajectorySequenceBuilder(new Pose2d(42, -61.4, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(36, -61.4, Math.toRadians(90)))
+                                .lineToSplineHeading(new Pose2d(36, -12, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(34, -4, Math.toRadians(-15)))
                                 .build()
                 );
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
