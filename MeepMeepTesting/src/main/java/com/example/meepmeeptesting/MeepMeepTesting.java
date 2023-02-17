8db5e311464de7e2cbd318cb15dbcac778c63090
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-02-17 15:46:12 -0500

    some changes

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,23 +1,9 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.drive.MecanumDrive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
-import com.acmerobotics.roadrunner.trajectory.constraints.MinAccelerationConstraint;
-import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
-import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
-import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Vector;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -27,10 +13,48 @@ public class MeepMeepTesting {
                 .setDimensions(13, 18)
                 .setConstraints(60, 40, 10, Math.toRadians(180), 10.5553)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(42, -61.4, Math.toRadians(90)))
-                                .lineToLinearHeading(new Pose2d(36, -61.4, Math.toRadians(90)))
-                                .lineToSplineHeading(new Pose2d(36, -12, Math.toRadians(90)))
-                                .lineToLinearHeading(new Pose2d(34, -4, Math.toRadians(-15)))
+                        drive.trajectorySequenceBuilder(new Pose2d(40.5, 63.5, Math.toRadians(270)))
+                                //first cone
+                                .splineToLinearHeading(new Pose2d(42, 58.5, toRAD(270)), toRAD(270))
+                                .lineToLinearHeading(new Pose2d(12, 58.5, toRAD(270)))
+                                .addTemporalMarker(()->{})
+                                .lineToLinearHeading(new Pose2d(12, 11.5, toRAD(270)))
+                                .lineToLinearHeading(new Pose2d(24.5, 11.5, Math.toRadians(270)))
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.25)
+                                .lineToLinearHeading(new Pose2d(26.5, 7.5, Math.toRadians(270)))
+                                .waitSeconds(0.1)
+                                .addTemporalMarker(()->{})
+                                //second cone
+                                .setReversed(true)
+                                .lineToLinearHeading(new Pose2d(23.5, 12.5, Math.toRadians(270)))
+                                .waitSeconds(0.1)
+                                .addTemporalMarker(() -> {
+
+                                })
+                                .waitSeconds(0.25)
+                                .lineToLinearHeading(new Pose2d(63.5, 12, Math.toRadians(0)))
+                                .addTemporalMarker(() -> {
+
+                                })
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {
+
+                                })
+                                .waitSeconds(0.25)
+                                .addTemporalMarker(() -> {
+
+                                })
+                                .lineToLinearHeading(new Pose2d(24, 14, Math.toRadians(270)))
+                                .addTemporalMarker(() -> {
+
+                                })
+                                .lineToLinearHeading(new Pose2d(26.5, 7.25, Math.toRadians(270)))
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {
+
+                                })
+                                .waitSeconds(0.25)
                                 .build()
                 );
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
