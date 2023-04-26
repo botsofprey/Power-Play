9a30ded124e0a8c402d862f6a96609bfe12081b3
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-04-26 16:22:08 -0400

    some changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Drive extends OpMode {
+    DriveControls controls = new DriveControls();
+    @Override
+    public void init(){
+        controls.getHardware(hardwareMap);
+    }
+    @Override
+    public void loop(){
+        double y = gamepad1.left_stick_y;
+        double x = gamepad1.right_stick_x;
+        double rx = gamepad1.right_stick_x;
+        controls.drive(y, x, rx);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControls.java
@@ -1,4 +1,59 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
 public class DriveControls {
+    IMU imu;
+    private final DcMotor[] motors = new DcMotor[4];
+    private static final String[] MOTOR_NAMES = {
+            "frontLeftDriveMotor",
+            "backLeftDriveMotor",
+            "frontRightDriveMotor",
+            "backRightDriveMotor"
+    };
+    private final DcMotorSimple.Direction[] directions = {
+            DcMotorSimple.Direction.REVERSE,
+            DcMotorSimple.Direction.REVERSE,
+            DcMotorSimple.Direction.FORWARD,
+            DcMotorSimple.Direction.FORWARD
+    };
+    public void getHardware(HardwareMap hw){
+        IMU.Parameters myIMUparameters;
+        myIMUparameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                )
+        );
+        imu.initialize(myIMUparameters);
+        for (int i = 0; i < motors.length; i++) {
+            motors[i] = hw.get(DcMotor.class, MOTOR_NAMES[i]);
+            motors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motors[i].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            motors[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motors[i].setDirection(directions[i]);
+        }
+    }
+public double getAngle(AngleUnit unit){
+    YawPitchRollAngles robotOrientation;
+    robotOrientation = imu.getRobotYawPitchRollAngles();
+        return robotOrientation.getYaw(unit);
+}
+
+public void drive(double y, double x, double rx){
+    double rotX = x * Math.cos(-getAngle(AngleUnit.RADIANS)) - y * Math.sin(-getAngle(AngleUnit.RADIANS));
+    double rotY = x * Math.sin(-getAngle(AngleUnit.RADIANS)) + y * Math.cos(-getAngle(AngleUnit.RADIANS));
+    double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+    motors[1].setPower((rotY + rotX + rx) / denominator);
+    motors[2].setPower((rotY - rotX + rx) / denominator);
+    motors[3].setPower((rotY - rotX - rx) / denominator);
+    motors[4].setPower((rotY + rotX - rx) / denominator);
+    }
 }
